f66c84d772afcabf9dc9da2343fd659dc4645e0b
Onur Yildirim <onurix@hotmail.com>
2019-01-31 13:04:41 +0200

    main.py update
    main update

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ import modbus_tk_.modbus_rtu as modbus_rtu
 
 #logger = modbus_tk_.utils.create_logger("console")
 
-master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyO0', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+master = modbus_rtu.RtuMaster(serial.Serial(port='COM4', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
 master.set_timeout(3.0)
 master.set_verbose(True)
 a=0
-while a<10:
-    data = master.execute(2, cst.READ_HOLDING_REGISTERS, 0, 8)
+while a<1000:
+    data = master.execute(2, cst.READ_HOLDING_REGISTERS, 1000, 8)
+    print data
     time.sleep(0.15)
-    data = master.execute(2, cst.WRITE_SINGLE_REGISTER, 0, output_value=1)
+    data = master.execute(2, cst.WRITE_MULTIPLE_REGISTERS, 2000, output_value=xrange(12))
     time.sleep(0.15)
+    a = a + 1
 
