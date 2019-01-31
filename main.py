5f62f3aafe0654ba53ad558d474a87b2443b12b8
root <root@beaglebone.localdomain>
2019-01-31 06:26:18 +0000

    modified:   main.py 	modbus_tk_/__init__.pyc 	modbus_tk_/defines.pyc 	modbus_tk_/hooks.pyc 	modbus_tk_/modbus.pyc 	modbus_tk_/modbus_rtu.pyc 	modbus_tk_/utils.pyc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import package
-import paho.mqtt.client as mqtt
+#import paho.mqtt.client as mqtt
 import time
 import serial
 #add logging capability
@@ -11,12 +11,12 @@ import modbus_tk_.modbus_rtu as modbus_rtu
 
 #logger = modbus_tk_.utils.create_logger("console")
 
-master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyUSB0', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+master = modbus_rtu.RtuMaster(serial.Serial(port='/dev/ttyO0', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
 master.set_timeout(3.0)
 master.set_verbose(True)
 a=0
 while a<10:
-    data = master.execute(2, defines.READ_HOLDING_REGISTERS, 0, 8)
+    data = master.execute(2, cst.READ_HOLDING_REGISTERS, 0, 8)
     time.sleep(0.15)
     data = master.execute(2, cst.WRITE_SINGLE_REGISTER, 0, output_value=1)
     time.sleep(0.15)
